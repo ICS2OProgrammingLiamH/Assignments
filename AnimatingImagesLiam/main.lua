1022f68c940515added68ceb6a4d83aa62436e96
LiamHoltzhauer20 <43419313+LiamHoltzhauer20@users.noreply.github.com>
2018-10-10 21:05:24 -0400

    Animating images

--- AnimatingImagesLiam/main.lua
+++ AnimatingImagesLiam/main.lua
@@ -1,7 +1,15 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: Animating images
+-- Name: Liam H
+-- Course: ICS20
+-- This program animates 3 images at once
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--  global variables
+scrollspeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/space.png", 2048, 1536)
