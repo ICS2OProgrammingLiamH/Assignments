b91f0646f85a0cfb2c77e60b7c57554215a96db5
LiamHoltzhauer20 <43419313+LiamHoltzhauer20@users.noreply.github.com>
2018-10-30 13:52:22 -0400

    Assingment

---
+++ Assignment 5/main.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------------------------------------
+-- Title: Math quiz
+-- Name: Liam H
+-- Course: ICS2O
+-- this program is a math game and when you get an anser right you get a point
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- local variables 
+local RandomOperator = 1
+local RandomOperator2 = 2
+local RandomOperator3 = 3
+local RandomOperator4 = math.random 
+
